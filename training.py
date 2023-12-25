d7fd4551946231c88e3e8184fda3cc6a7c48a1c8
dongjaekim-hail <112673757+dongjaekim-hail@users.noreply.github.com>
2023-12-25 22:38:33 +0900

    Add files via upload
    up to date for running version

--- training.py
+++ training.py
@@ -50,6 +50,7 @@ def preprocess_data(examples):
             answer_str = "예"
         else:
             answer_str = "아니요"
+        answer_str = examples["clue_text"][i]
 
         # context 내에서 해당 문자열의 위치 찾기
         start_position = examples["context"][i].find(answer_str)
@@ -60,6 +61,10 @@ def preprocess_data(examples):
 
     tokenized_inputs["start_positions"] = start_positions
     tokenized_inputs["end_positions"] = end_positions
+
+    # for those who start_positions == -1, remove the data from tokenized_inputs
+    # tokenized_inputs = {key: [value[i] for i in range(len(start_positions)) if start_positions[i] != -1] for key, value in tokenized_inputs.items()}
+
     return tokenized_inputs
 
 
@@ -90,8 +95,53 @@ class CustomTrainer(Trainer):
 trainer = CustomTrainer(
     model=model,
     args=training_args,
-    train_dataset=encoded_dataset["train"],
+    train_dataset=encoded_dataset["training"],
     eval_dataset=encoded_dataset["validation"],
 )
 
 trainer.train()
+
+# get inference from the trained model
+inferred_output=trainer.predict(encoded_dataset["validation"])
+
+import numpy as np
+
+# Assuming inferred_output is the result from trainer.predict
+predictions = inferred_output.predictions
+
+# Convert logits to probabilities (softmax)
+start_logits, end_logits = predictions
+start_probs = torch.softmax(torch.tensor(start_logits), dim=-1).numpy()
+end_probs = torch.softmax(torch.tensor(end_logits), dim=-1).numpy()
+
+# Find the token positions with the highest start and end probabilities
+start_indexes = np.argmax(start_probs, axis=1)
+end_indexes = np.argmax(end_probs, axis=1)
+
+answers = []
+
+for i, (start_index, end_index) in enumerate(zip(start_indexes, end_indexes)):
+    # Map token positions to character positions in the original context
+    context = encoded_dataset["validation"]["context"][i]
+    # Extract the answer text
+    answer = context[start_index:end_index+ 1]
+    answers.append(answer)
+
+clue_text = encoded_dataset["validation"]["clue_text"]
+answer_percent = []
+
+# comparison between the predicted answer and the clue_text, just showing
+for count, answer in enumerate(answers):
+    print("Predicted answer: \n")
+    print(answer)
+    print("Clue text: \n")
+    print(clue_text[count])
+    # calculate the overlap score
+    overlap_score = len(set(answer) & set(clue_text[count])) / len(set(answer) | set(clue_text[count]))
+    print("\t\tOverlap score: ", overlap_score)
+    print("=====================================")
+    answer_percent.append(overlap_score)
+
+
+# final score
+print("Final score: ", np.mean(answer_percent))
